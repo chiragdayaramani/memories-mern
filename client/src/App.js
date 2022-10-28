34a91e6f36ca632788d8f83ff371646227fff77a
CHIRAG <chiragddayaramani@gmail.com>
2022-10-28 19:42:01 +0530

    added styles files

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,17 @@ import memories from './images/memories.png'
 import Posts from './components/Posts/Posts.js';
 import Form from './components/Form/Form.js';
 
+import useStyles from './styles';
+
+
 const App =()=>{
+    const classes=useStyles();
+
     return(
         <Container maxwidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography varient="h2" align="center">Memories</Typography>
-                <img src={memories} alt="memories" height="500"/>
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60"/>
             </AppBar>
             <Grow in>
                 <Container>
