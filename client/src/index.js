e16ec1349817ce51966dfa28f2b51a1db2f8085c
CHIRAG <chiragddayaramani@gmail.com>
2022-11-26 16:58:42 +0530

    setup redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
+import { Provider } from "react-redux";
+import { configureStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import reducers from './reducers';
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+import App from "./App";
+
+const strore = configureStore(reducers,compose(applyMiddleware(thunk)));
+
+ReactDOM.render(<App />, document.getElementById("root"));
